6b5c5b6db04ff344822b78070ae2daccd6fc3729
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-08-27 11:40:23 -0400

    Adjust redirect URI
    This commit primarily changes the redirect URI to be
    localhost:7165/accounts and makes some minor content changes to the
    comments

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,19 +6,18 @@ class AlbumsController < ApplicationController
   before_action :set_album, only: %i[show update destroy]
 
   # GET /albums
-  # This works, test it in incognito mode with:
-  # https://accounts.spotify.com/authorize/?client_id=c4b89345becb45f7b9289414554c088c&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4741%2Fcallback&scope=user-read-email&state=abcdefghijklmnopqrstuvwxyz0123456789
+  # This hard-coded link works, test it in incognito mode with:
+  # https://accounts.spotify.com/authorize/?client_id=c4b89345becb45f7b9289414554c088c&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A7165%2Faccount&scope=user-read-email&state=abcdefghijklmnopqrstuvwxyz0123456789
   def index
-    @albums = HTTParty.get('https://accounts.spotify.com/authorize',
+    render json: HTTParty.get('https://accounts.spotify.com/authorize',
       :headers => { 'Content-Type' => 'application/json' },
       query: {
         'client_id' => ENV['SPOTIFY_CLIENT_ID'],
         'response_type' => 'code',
-        'redirect_uri' => 'http://localhost:4741/callback',
+        'redirect_uri' => 'http://localhost:7165/account',
         'scope' => 'user-read-private user-read-email',
         'state' => 'abcdefghijklmnopqrstuvwxyz0123456789'
       })
-    render json: @albums
   end
 
   def example
@@ -29,17 +28,13 @@ class AlbumsController < ApplicationController
         'client_secret' => ENV['SPOTIFY_CLIENT_SECRET'],
         'grant_type' => 'authorization_code',
         'code' => params['code'],
-        'redirect_uri' => 'http://localhost:4741/callback'
+        'redirect_uri' => 'http://localhost:7165/account'
       })
     render json: @access
   end
 
-  def data
-    redirect_to 'https://api.spotify.com/v1/me?access_token=', @access.response.body['access_token']
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use accounts to share common setup or constraints between actions.
     def set_album
       @album = Album.find(params[:id])
     end
