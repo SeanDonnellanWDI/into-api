ecc211e0affeaab97cc01d8432f25cfb51b2135a
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-09-07 15:04:28 -0400

    Ok that didn't work, next test
    For the record, I'm doing these live tests because I've been able to do this in my dev environment but I cannot get Spotify auth to persist in my live environment. So I'm testing several different approaches into the live envi

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -63,7 +63,7 @@ class AccountsController < ProtectedController
                               'access_token' => @sp_access_token
                             })
     @sp_user_email = @sp_data['email']
-    Account.errors.full_messages
+    @user.errors.full_messages
     Account.create(user_id: @user.id,
                    service: @sp_service,
                    username: @sp_user_email)
