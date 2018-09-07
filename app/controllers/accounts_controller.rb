a75b8936e81294148012b0bd6866b378969870a8
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-09-07 16:38:07 -0400

    puts 'this sp_data is', @sp_data

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -63,7 +63,7 @@ class AccountsController < ProtectedController
                               'access_token' => @sp_access_token
                             })
     @sp_user_email = @sp_data['email']
-    puts 'this sp_data is' + @sp_data
+    puts 'this sp_data is', @sp_data
     Account.create(user_id: @user.id,
                    service: @sp_service,
                    username: @sp_user_email)
