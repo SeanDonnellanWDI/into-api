4454ea2735cbb0a0ac4f1a59b47076df8515fc13
SeanDonnellanWDI <sean.donnellan.wdi@gmail.com>
2018-09-11 16:33:38 -0400

    Cleanup code, remove puts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -45,34 +45,22 @@ class AccountsController < ProtectedController
   # GET /callback
   def callback
     @sp_service = 'Spotify'
-    puts "111 @sp_service is #{@sp_service}"
     @sp_code = params['code']
-    puts "222 @sp_code is #{@sp_code}"
     @user = User.find(params['state'])
-    puts "333 @user is #{@user}"
-    @client_id = ENV['SPOTIFY_CLIENT_ID']
-    puts "333333 client_id is #{@client_id}"
-    @client_secret = ENV['SPOTIFY_CLIENT_SECRET']
-    puts "333333333 client_secret is #{@client_secret}"
     @sp_access = HTTParty.post('https://accounts.spotify.com/api/token',
                                headers: { 'Accept' => 'application/json' },
                                body: {
-                                 'client_id' => @client_id,
-                                 'client_secret' => @client_secret,
+                                 'client_id' => ENV['SPOTIFY_CLIENT_ID'],
+                                 'client_secret' => ENV['SPOTIFY_CLIENT_SECRET'],
                                  'grant_type' => 'authorization_code',
                                  'code' => @sp_code,
                                  'redirect_uri' => 'https://into-api.herokuapp.com/callback'
                                })
-    puts "444 @sp_access is #{@sp_access}"
     @sp_parse_access = JSON.parse(@sp_access.body)
-    puts "555 @sp_parse_access is #{@sp_parse_access}"
     @sp_access_token = @sp_access['access_token']
-    puts "666 @sp_access_token is #{@sp_access_token}"
     @sp_data = HTTParty.get('https://api.spotify.com/v1/me',
                             headers: { 'Authorization' => "Bearer #{@sp_access_token}", 'Accept' => 'application/json' })
-    puts "777 @sp_data is #{@sp_data}"
     @sp_user_email = @sp_data['email']
-    puts "888 @sp_user_email is #{@sp_user_email}"
     Account.create(user_id: @user.id,
                    service: @sp_service,
                    username: @sp_user_email)
